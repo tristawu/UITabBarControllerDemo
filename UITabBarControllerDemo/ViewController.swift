a3a287a73e8c65e27c637a2d33329344b5f2ce68
TristaWu <trista@kcdgs.com>
2021-02-10 20:00:40 +0800

    Initial Commit

---
+++ UITabBarControllerDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITabBarControllerDemo
+//
+//  Created by Trista on 2021/2/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
